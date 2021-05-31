6a7c2a8cfa5545ad0146d3b14b4790c54dac1df9
mashiko <mashiko@technopower.co.jp>
2021-05-31 10:39:20 +0900

    ブログ完成！

--- SNS_project/blog/models.py
+++ SNS_project/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=40)
     text = models.TextField()
+    image = models.ImageField(null = True , upload_to = "media/images/")
     posted_at = models.DateField()
     updated_at = models.DateField()
 
